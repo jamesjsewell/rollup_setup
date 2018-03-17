0f2c1026e926cda7e65b90c2da84c9710ff450e9
james sewell <jamesjosephsewell@gmail.com>
2018-03-17 13:28:39 -0500

    now uses npm-run-all to run scripts in parallel in one tab instead of using ttab for multiple tabs, thanks @tezzutezzu on twitter for the suggestion. Also there was a typo bug in the build script that i fixed

--- compiled.js
+++ compiled.js
@@ -1,2 +1,2 @@
-!function(){"use strict";console.log(5,"should equal 5"),console.log("test")}();
+!function(){"use strict";console.log(5,"should equal 5"),console.log("test"),console.log("test2")}();
 //# sourceMappingURL=compiled.js.map
