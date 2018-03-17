2082b394d5b7c7caa59253a50c7fc78646080c2d
james sewell <jamesjosephsewell@gmail.com>
2018-03-16 23:40:25 -0500

    updated package.json a bit

--- compiled.js
+++ compiled.js
@@ -1,13 +1,2 @@
-(function () {
-'use strict';
-
-var aNumber = 4;
-
-//importing a number from another javascript file, you could import anything, like a class for example
-
-//this is showing that import/export of es6 modules is working in the browser
-console.log(aNumber + 1, 'should equal 5');
-console.log('test');
-
-}());
+!function(){"use strict";console.log(5,"should equal 5"),console.log("test")}();
 //# sourceMappingURL=compiled.js.map
