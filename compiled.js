62d333df95e03d82de7ef3c8f3d047b43e1cada5
james sewell <jamesjosephsewell@gmail.com>
2018-03-18 15:46:12 -0500

    renamed a folder to a more appropriate name

--- compiled.js
+++ compiled.js
@@ -1,2 +1,15 @@
-!function(){"use strict";console.log(5,"should equal 5"),console.log("test"),console.log("test2")}();
+(function () {
+'use strict';
+
+var aNumber = 4;
+
+//importing a number from another javascript file, you could import anything, like a class for example
+
+//this is showing that import/export of es6 modules is working in the browser
+console.log(aNumber + 1, 'should equal 5');
+console.log('test');
+console.log('test2');
+console.log('test3');
+
+}());
 //# sourceMappingURL=compiled.js.map
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //importing a number from another javascript file, you could import anything, like a class for example
-import aNumber from './code_splitting_example/some_code.js'
+import aNumber from './some_code/some_code.js'
 
 //this is showing that import/export of es6 modules is working in the browser
 console.log(aNumber+1, 'should equal 5')
 console.log('test')
 console.log('test2')
+console.log('test3')
 
 
     
